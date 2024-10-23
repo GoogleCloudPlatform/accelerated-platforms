9acedd43023ca19e7927e8e6fcf9b8009b0c3e55
Jun Sheng <jscheng@google.com>
2024-10-23 22:54:09 +1100

    move provider

--- use-cases/rag-on-gke/alloyDB/init_database.tf
+++ use-cases/rag-on-gke/alloyDB/init_database.tf
@@ -1,3 +1,18 @@
+data "google_client_config" "provider" {}
+
+data "google_container_cluster" "my_cluster" {
+  name     = var.gke_cluster_name
+  location = var.gke_cluster_location
+  project  = var.project_id
+}
+
+provider "kubernetes" {
+  host  = "https://${data.google_container_cluster.my_cluster.endpoint}"
+  token = data.google_client_config.provider.access_token
+  cluster_ca_certificate = base64decode(
+    data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate,
+  )
+}
 
 
 resource "google_service_account_iam_binding" "admin-account-iam" {
--- use-cases/rag-on-gke/alloyDB/kube-psql-job/psql-job.tf
+++ use-cases/rag-on-gke/alloyDB/kube-psql-job/psql-job.tf
@@ -1,18 +1,3 @@
-data "google_client_config" "provider" {}
-
-data "google_container_cluster" "my_cluster" {
-  name     = var.gke_cluster_name
-  location = var.gke_cluster_location
-  project  = var.project_id
-}
-
-provider "kubernetes" {
-  host  = "https://${data.google_container_cluster.my_cluster.endpoint}"
-  token = data.google_client_config.provider.access_token
-  cluster_ca_certificate = base64decode(
-    data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate,
-  )
-}
 
 resource "kubernetes_config_map" "get-token" {
   metadata {

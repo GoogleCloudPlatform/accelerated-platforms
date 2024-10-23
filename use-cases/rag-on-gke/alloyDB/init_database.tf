c5fd3192388e1d8e1c83b34dbe1f10e4345b999b
Jun Sheng <jscheng@google.com>
2024-10-23 20:30:22 +1100

    tentative modulize

---
+++ use-cases/rag-on-gke/alloyDB/init_database.tf
@@ -0,0 +1,11 @@
+module "createdb" {
+  source = "kube-psql-job"
+  project_id = var.project_id
+  name = "init-test"
+  gke_cluster_name = "gpu-ml"
+  gke_cluster_location = "asia-southeast1-a"
+  sql_script = "select current_user;"
+  environs = {}
+  pghost = "127.0.0.1"
+  pgdatabase = "postgres"
+}

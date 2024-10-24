3370ce6764330971f47eb41983b1da80ed18e8b6
Jun Sheng <jscheng@google.com>
2024-10-24 17:00:00 +1100

    Fix privilige on public schema

--- use-cases/rag-on-gke/alloyDB/init_database_objects/init_database.tf
+++ use-cases/rag-on-gke/alloyDB/init_database_objects/init_database.tf
@@ -105,6 +105,9 @@ module "create-extension" {
   GRANT SELECT, INSERT, UPDATE, DELETE
     ON ALL TABLES IN SCHEMA google_ml
     TO "alloydb-raguser@${var.project_id}.iam";
+  GRANT ALL ON SCHEMA public
+    TO "alloydb-raguser@${var.project_id}.iam";
+
 EOT
   environs = {}
   pghost = data.external.alloydb-primary-instance-ip.result.ipAddress

d8b7717c48ecea7240ff03809cd4d451aad17712
Aaron Rueth <rueth@google.com>
2024-10-29 07:44:18 -0700

    Updated google Terraform providers (#21)

--- terraform/modules/cluster/versions.tf
+++ terraform/modules/cluster/versions.tf
@@ -16,11 +16,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "6.7.0"
+      version = "6.9.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "6.7.0"
+      version = "6.9.0"
     }
   }
 }

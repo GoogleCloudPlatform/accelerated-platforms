506691df9f3c62a5067c3fa2093c77fcd63243b8
gushob21 <gushob@google.com>
2024-11-01 15:42:35 +0000

    refining guide

--- use-cases/inferencing/serving-with-vllm/benchmarks/src/locust-tasks/tasks.py
+++ use-cases/inferencing/serving-with-vllm/benchmarks/src/locust-tasks/tasks.py
@@ -1,12 +1,10 @@
-#!/usr/bin/env python
-
-# Copyright 2022 Google Inc. All rights reserved.
+# Copyright 2024 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,9 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-from datetime import datetime
-from locust import FastHttpUser, TaskSet, task, between
+from locust import FastHttpUser, run_single_user, task, between
 import logging
 import logging.config
 import os
@@ -30,20 +26,19 @@ def graceful_shutdown(signal_number, stack_frame):
     # TODO: Add logic to handled checkpointing if required
     sys.exit(0)
 
-# [START locust_test_task]
 
-class MetricsTaskSet(TaskSet):
+class MyUser(FastHttpUser):
 
-    #wait_time = between(1, 5)
-    model_id = None
-    message1 = None
-    message2 = None
-
-    def on_start(self):
+    def __init__(self):  # Constructor
         self.model_id = os.environ["MODEL_ID"]
+        self.endpoint = os.environ["ENDPOINT"]
+        self.host = os.environ["HOST"]
         self.message1 = ( "I'm looking for comfortable cycling shorts for women, what are some good options?")
         self.message2 = "Tell me about some tops for men, looking for different styles"
-                             
+
+    wait_time = between(1, 5)
+    
+
     @task(50)
     def test1(self):
         headers = {'content-type': 'application/json'}
@@ -69,18 +64,31 @@ class MetricsTaskSet(TaskSet):
                 "max_tokens": 256,
             },headers=headers) 
         print("FROM MESSAGE 2",r)       
+    def benchmarks(self):
+        # if "ACTION" in os.environ and os.environ["ACTION"] == "benchmark":
+        #     self.test1()
+        #     self.test2()
+        self.test1()
+        self.test2()
 
+if __name__ == "__main__":
+    # Configure logging
+    logging.config.fileConfig("logging.conf")
 
+    logger = logging.getLogger("benchmark_obj")
 
-class MetricsLocust(FastHttpUser):
+    if "LOG_LEVEL" in os.environ:
+        new_log_level = os.environ["LOG_LEVEL"].upper()
+        logger.info(
+            f"Log level set to '{new_log_level}' via LOG_LEVEL environment variable"
+        )
+        logging.getLogger().setLevel(new_log_level)
+        logger.setLevel(new_log_level)
 
-    # def __init__(self):  # Constructor
-    #     self.model_id = os.environ["MODEL_ID"]
-    #     self.endpoint = os.environ["ENDPOINT"]
-    #     #self.host = os.environ["HOST"]
-    #     self.message1 = ( "I'm looking for comfortable cycling shorts for women, what are some good options?")
-    #     self.message2 = "Tell me about some tops for men, looking for different styles"
-    
-    tasks = {MetricsTaskSet}
-
-# [END locust_test_task]
+    logger.info("Configure signal handlers")
+    signal.signal(signal.SIGINT, graceful_shutdown)
+    signal.signal(signal.SIGTERM, graceful_shutdown)
+    benchmark_obj = MyUser()
+    benchmark_obj.benchmarks()
+    #benchmark_obj.test1()
+    #benchmark_obj.test2()

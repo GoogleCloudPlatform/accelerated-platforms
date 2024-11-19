4c5b75097f8aae4ed80d39178e7fe3ac95713667
gushob21 <gushob@google.com>
2024-11-19 19:24:53 +0000

    Adding HyperdiskML and fixing READMEs

--- test/scripts/qwiklabs/playground_byop_oci_destroy.sh
+++ test/scripts/qwiklabs/playground_byop_oci_destroy.sh
@@ -26,8 +26,6 @@ source ${SCRIPTS_DIR}/helpers/include.sh
 echo_title "Checking prerequisites"
 source ${SCRIPTS_DIR}/helpers/bin_crane.sh
 
-export terraform_command="destroy"
-
 echo_title "Preparing the environment"
 source ${SCRIPTS_DIR}/helpers/byop_env.sh
 source ${SCRIPTS_DIR}/helpers/${MLP_TYPE}_env.sh

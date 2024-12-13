19b2fbcba8ed5a112f8c5eb328667bf72a2876fe
Aaron Rueth <rueth@google.com>
2024-12-13 17:51:40 +0000

    Added extensibility to the deploy and teardown scripts

--- platforms/gke/base/core/teardown.sh
+++ platforms/gke/base/core/teardown.sh
@@ -17,9 +17,7 @@ set -o errexit
 
 start_timestamp=$(date +%s)
 
-terraservices=("workloads/kueue" "gke_enterprise/fleet_membership" "container_node_pool" "container_cluster" "networking")
-# Disable gke_enterprise/servicemesh due to b/376312292
-#terraservices=("workloads/kueue" "gke_enterprise/servicemesh" "gke_enterprise/fleet_membership" "container_node_pool" "container_cluster" "networking")
+declare -a terraservices=${CORE_TERRASERVICES_DESTROY:-("workloads/kueue" "gke_enterprise/fleet_membership" "container_node_pool" "container_cluster" "networking")}
 
 source ${ACP_PLATFORM_BASE_DIR}/_shared_config/scripts/set_environment_variables.sh ${ACP_PLATFORM_BASE_DIR}/_shared_config
 
@@ -42,17 +40,16 @@ done
 
 cd ${ACP_PLATFORM_CORE_DIR}/initialize &&
     echo "Current directory: $(pwd)" &&
-    cp backend.tf.local backend.tf &&
+    rm -rf backend.tf &&
     terraform init -force-copy -lock=false -migrate-state || exit 1
 gcloud storage rm -r gs://${ACP_TERRAFORM_BUCKET_NAME}/* &&
     terraform destroy -auto-approve || exit 1
-rm -rf .terraform/ state/
 
 rm -rf \
     ${ACP_PLATFORM_BASE_DIR}/_shared_config/.terraform/ \
     ${ACP_PLATFORM_BASE_DIR}/_shared_config/terraform.tfstate* \
     ${ACP_PLATFORM_CORE_DIR}/initialize/.terraform/ \
-    ${ACP_PLATFORM_CORE_DIR}/initialize/state/default.tfstate* \
+    ${ACP_PLATFORM_CORE_DIR}/initialize/terraform.tfstate* \
     ${ACP_PLATFORM_CORE_DIR}/networking/.terraform/ \
     ${ACP_PLATFORM_CORE_DIR}/container_cluster/.terraform/ \
     ${ACP_PLATFORM_CORE_DIR}/container_node_pool/.terraform/ \
@@ -66,17 +63,8 @@ rm -rf \
     ${ACP_PLATFORM_CORE_DIR}/workloads/manifests
 
 git restore \
-    ${ACP_PLATFORM_CORE_DIR}/initialize/backend.tf \
     ${ACP_PLATFORM_CORE_DIR}/initialize/backend.tf.bucket \
-    ${ACP_PLATFORM_CORE_DIR}/networking/backend.tf \
-    ${ACP_PLATFORM_CORE_DIR}/container_cluster/backend.tf \
-    ${ACP_PLATFORM_CORE_DIR}/container_node_pool/backend*.tf \
-    ${ACP_PLATFORM_CORE_DIR}/container_node_pool/container_node_pool_*.tf \
-    ${ACP_PLATFORM_CORE_DIR}/gke_enterprise/configmanagement/git/backend.tf \
-    ${ACP_PLATFORM_CORE_DIR}/gke_enterprise/configmanagement/oci/backend.tf \
-    ${ACP_PLATFORM_CORE_DIR}/gke_enterprise/fleet_membership/backend.tf \
-    ${ACP_PLATFORM_CORE_DIR}/gke_enterprise/servicemesh/backend.tf \
-    ${ACP_PLATFORM_CORE_DIR}/workloads/kueue/backend.tf
+    ${ACP_PLATFORM_CORE_DIR}/container_node_pool/container_node_pool_*.tf
 
 end_timestamp=$(date +%s)
 total_runtime_value=$((end_timestamp - start_timestamp))

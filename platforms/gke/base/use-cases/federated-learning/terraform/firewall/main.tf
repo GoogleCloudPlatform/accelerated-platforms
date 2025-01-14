ed3cd5f413b9c7b4932bde25d0b801c93f6953dd
Marco Ferrari <ferrarim@google.com>
2025-01-14 17:37:50 +0100

    feat: configure the federated learning use case (#74)
    - configure firewall for federated learning
    - configure iam roles and service accounts
    - configure dedicated node pools
    - configure policy controller and policies
    - configure dedicated Kubernetes namespaces

---
+++ platforms/gke/base/use-cases/federated-learning/terraform/firewall/main.tf
@@ -0,0 +1,165 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+resource "google_compute_network_firewall_policy" "federated_learning_fw_policy" {
+  description = "Federated learning firewall policy"
+  name        = "${local.cluster_name}-federated-learning-firewall-policy"
+  project     = data.google_project.default.project_id
+}
+
+resource "google_compute_network_firewall_policy_association" "federated_learning_vpc_associations" {
+  name              = "${local.cluster_name}-federated-learning-firewall-policy-association"
+  attachment_target = data.google_compute_network.main_vpc_network.id
+  firewall_policy   = google_compute_network_firewall_policy.federated_learning_fw_policy.name
+  project           = data.google_project.default.project_id
+}
+
+resource "google_compute_network_firewall_policy_rule" "federated_learning_fw_rule_deny_all" {
+  action                  = "deny"
+  description             = "Default deny egress from node pools"
+  direction               = "EGRESS"
+  enable_logging          = true
+  firewall_policy         = google_compute_network_firewall_policy.federated_learning_fw_policy.name
+  priority                = 65535
+  project                 = data.google_project.default.project_id
+  rule_name               = "${local.cluster_name}-node-pools-deny-egress"
+  target_service_accounts = local.node_pool_service_account_emails
+
+  match {
+    dest_ip_ranges = ["0.0.0.0/0"]
+
+    layer4_configs {
+      ip_protocol = "all"
+    }
+  }
+}
+
+resource "google_compute_network_firewall_policy_rule" "federated_learning_fw_rule_allow_egress_to_cluster" {
+  action                  = "allow"
+  description             = "Allow egress from node pools to cluster nodes, pods and services"
+  direction               = "EGRESS"
+  enable_logging          = true
+  firewall_policy         = google_compute_network_firewall_policy.federated_learning_fw_policy.name
+  priority                = 1000
+  project                 = data.google_project.default.project_id
+  rule_name               = "${local.cluster_name}-node-pools-allow-egress-nodes-pods-services"
+  target_service_accounts = local.node_pool_service_account_emails
+
+  match {
+    dest_ip_ranges = concat(
+      local.cluster_subnetwork_secondary_ip_ranges,
+      [data.google_container_cluster.cluster.services_ipv4_cidr]
+    )
+
+    layer4_configs {
+      ip_protocol = "all"
+    }
+  }
+}
+
+resource "google_compute_network_firewall_policy_rule" "federated_learning_fw_rule_allow_egress_to_k8s_api" {
+  action                  = "allow"
+  description             = "Allow egress from node pools to the Kubernetes API server"
+  direction               = "EGRESS"
+  enable_logging          = true
+  firewall_policy         = google_compute_network_firewall_policy.federated_learning_fw_policy.name
+  priority                = 1001
+  project                 = data.google_project.default.project_id
+  rule_name               = "${local.cluster_name}-node-pools-allow-egress-api-server"
+  target_service_accounts = local.node_pool_service_account_emails
+
+  match {
+    dest_ip_ranges = [local.master_ipv4_cidr_block]
+
+    layer4_configs {
+      ip_protocol = "tcp"
+      ports       = [443, 10250]
+    }
+  }
+}
+
+resource "google_compute_network_firewall_policy_rule" "federated_learning_fw_rule_allow_egress_to_private_google_access" {
+  action                  = "allow"
+  description             = "Allow egress from node pools to Google APIs via Private Google Access"
+  direction               = "EGRESS"
+  enable_logging          = true
+  firewall_policy         = google_compute_network_firewall_policy.federated_learning_fw_policy.name
+  priority                = 1002
+  project                 = data.google_project.default.project_id
+  rule_name               = "${local.cluster_name}-node-pools-allow-egress-api-server"
+  target_service_accounts = local.node_pool_service_account_emails
+
+  match {
+    dest_ip_ranges = data.google_netblock_ip_ranges.private_google_access_netblock_ip_range.cidr_blocks_ipv4
+
+    layer4_configs {
+      ip_protocol = "tcp"
+    }
+  }
+}
+
+resource "google_compute_network_firewall_policy_rule" "federated_learning_fw_rule_allow_intra_egress" {
+  action          = "allow"
+  description     = "Allow pods to communicate with each other and the control plane"
+  direction       = "EGRESS"
+  enable_logging  = true
+  firewall_policy = google_compute_network_firewall_policy.federated_learning_fw_policy.name
+  priority        = 1003
+  project         = data.google_project.default.project_id
+  rule_name       = "${local.cluster_name}-intra-cluster-egress"
+
+  target_service_accounts = concat(
+    local.node_pool_service_account_emails,
+    local.apps_service_account_emails
+  )
+
+  match {
+    dest_ip_ranges = compact(
+      concat(
+        [
+          local.master_ipv4_cidr_block,
+          data.google_compute_subnetwork.region.ip_cidr_range,
+          data.google_container_cluster.cluster.tpu_ipv4_cidr_block,
+        ],
+        local.cluster_subnetwork_secondary_ip_ranges
+      )
+    )
+
+    layer4_configs {
+      ip_protocol = "all"
+    }
+  }
+}
+
+resource "google_compute_network_firewall_policy_rule" "federated_learning_fw_rule_allow_control_plane_ingress" {
+  action          = "allow"
+  description     = "Allow control plane to connect to pods for admission controllers and webhooks"
+  direction       = "INGRESS"
+  enable_logging  = true
+  firewall_policy = google_compute_network_firewall_policy.federated_learning_fw_policy.name
+  priority        = 1004
+  project         = data.google_project.default.project_id
+  rule_name       = "${local.cluster_name}-control-plane-ingress-webhooks"
+
+  target_service_accounts = local.node_pool_service_account_emails
+
+  match {
+    src_ip_ranges = [local.master_ipv4_cidr_block]
+
+    layer4_configs {
+      ip_protocol = "tcp"
+      ports       = ["8443", "9443", "15017"]
+    }
+  }
+}

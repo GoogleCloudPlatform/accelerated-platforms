9773708d59578fe951f364c730319fc51e174428
gushob21 <gushob@google.com>
2025-01-21 17:40:01 +0000

    improving data preprocessing script

--- use-cases/model-fine-tuning-pipeline/data-processing/ray/src/datapreprocessing/ray_utils.py
+++ use-cases/model-fine-tuning-pipeline/data-processing/ray/src/datapreprocessing/ray_utils.py
@@ -112,7 +112,7 @@ class RayUtils:
         #TODO: make this comment generic
         self.logger.debug("Data Preparation started")
         start_time = time.time()
-        results = ray.get([self.process_data.remote(preprocessor, df=self.df[i], ray_worker_node_id=i) for i in range(len(self.df))])
+        results = ray.get([self.process_data.remote(preprocessor=preprocessor, df=self.df[i], ray_worker_node_id=i) for i in range(len(self.df))])
         duration = time.time() - start_time
         self.logger.debug(f"Data Preparation finished in {duration} seconds")
 

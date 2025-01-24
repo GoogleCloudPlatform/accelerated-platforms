bbe95414f3b1e7461d4c38e3e32d2f0d31fea060
gushob21 <gushob@google.com>
2025-01-24 19:14:25 +0000

    improving data preprocessing script

--- use-cases/model-fine-tuning-pipeline/data-processing/ray/tests/test_dataloader.py
+++ use-cases/model-fine-tuning-pipeline/data-processing/ray/tests/test_dataloader.py
@@ -5,13 +5,16 @@ from unittest.mock import patch
 import csv
 from src.datapreprocessing.dataloader import DataLoader
 
+
 class TestDataLoader(unittest.TestCase):
-    @patch('src.datapreprocessing.dataloader.pd.read_csv')
+    @patch("src.datapreprocessing.dataloader.pd.read_csv")
     def test_load_raw_data(self, read_csv_mock):
-        read_csv_mock.return_value = pd.DataFrame({'col1': [1, 2], 'col2': [3, 4]})
-        dataloader = DataLoader('fake_bucket','fake_path')
+        read_csv_mock.return_value = pd.DataFrame({"col1": [1, 2], "col2": [3, 4]})
+        dataloader = DataLoader("fake_bucket", "fake_path")
         df = dataloader.load_raw_data()
-        read_csv_mock.assert_called_with('gs://fake_bucket/fake_path')
-        assert_frame_equal(df,read_csv_mock.return_value)
-if __name__ == '__main__':
+        read_csv_mock.assert_called_with("gs://fake_bucket/fake_path")
+        assert_frame_equal(df, read_csv_mock.return_value)
+
+
+if __name__ == "__main__":
     unittest.main()

a097527a932bf5bb8235bb3313c9ac507a7bec23
gushob21 <gushob@google.com>
2025-01-27 16:38:57 +0000

    improving data preporcessing script

--- run_python_coverage_test.sh
+++ run_python_coverage_test.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-coverage run -m unittest discover -s use-cases/model-fine-tuning-pipeline/data-processing/ray/tests
+
+#To add a new direcotry to coverage tests, add it to coverage_dirs array. 
+#Corresponding source (folder containing src and test directories) must be added to .coveragerc in the root dorectory
+coverage_dirs = ("accelerated-platforms/use-cases/model-fine-tuning-pipeline/data-processing/ray/tests/")
+
+#Add the source (folder containing src and test directories) to PYTHONPATH so the modules are available when you run the tests
+source_dirs = ("accelerated-platforms/use-cases/model-fine-tuning-pipeline/data-processing/ray")
+
+
+for coverage_dir in "${coverage_dirs[@]}"; do
+    python -m coverage run -m unittest discover ${coverage_dir}
+done
+
+for source_dir in "${source_dirs[@]}"; do
+    export PYTHONPATH=$PYTHONPATH:${source_dir}
+done

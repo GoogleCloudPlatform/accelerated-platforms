c291d0cf2519b21ac3c58a57428b50a16e1653ff
gushob21 <gushob@google.com>
2025-01-27 17:22:40 +0000

    improving data preprocessing script

--- run_python_unittests.sh
+++ run_python_unittests.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-#python -m unittest discover -s use-cases/model-fine-tuning-pipeline/data-processing/ray/tests
-cd use-cases/model-fine-tuning-pipeline/data-processing/ray && python -m unittest discover tests
+
+#Add the source (folder containing src and test directories) to PYTHONPATH so the modules are available when you run the tests
+source_dirs=("use-cases/model-fine-tuning-pipeline/data-processing/ray")
+#Add the directories containing the unit tests cases to test_dirs array
+test_dirs=("use-cases/model-fine-tuning-pipeline/data-processing/ray/tests/")
+
+for source_dir in "${source_dirs[@]}"; do
+    export PYTHONPATH=$PYTHONPATH:${source_dir}
+done
+
+for test_dir in "${test_dirs[@]}"; do
+    python -m unittest ${test_dir}
+done

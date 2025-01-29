73c6da37d07fee1012bc5ff8eea6e6d139d2443b
gushob21 <gushob@google.com>
2025-01-29 20:06:41 +0000

    improving data preprocessing script

--- run_python_coverage_test.sh
+++ run_python_coverage_test.sh
@@ -2,11 +2,11 @@
 
 #To add a new direcotry to coverage tests, add it to source_dirs array. 
 #Add the source (folder containing src and test directories) to the array
-source_dirs=("use-cases/model-fine-tuning-pipeline/data-processing/ray")
+source_dirs=("use-cases/model-fine-tuning-pipeline/data-processing/ray/src")
 
 for source_dir in "${source_dirs[@]}"; do
     export PYTHONPATH=$PYTHONPATH:${source_dir}
-    python -m coverage run -m unittest discover "${source_dir}/tests"
+    python -m coverage run -m unittest discover "${source_dir}/../tests"
 done
 
 
--- run_python_unittests.sh
+++ run_python_unittests.sh
@@ -2,9 +2,9 @@
 
 #To add a new direcotry to unit tests, add it to source_dirs array. 
 #Add the source (folder containing src and test directories) to the array
-source_dirs=("use-cases/model-fine-tuning-pipeline/data-processing/ray")
+source_dirs=("use-cases/model-fine-tuning-pipeline/data-processing/ray/src")
 
 for source_dir in "${source_dirs[@]}"; do
     export PYTHONPATH=$PYTHONPATH:${source_dir}
-    python -m unittest discover "${source_dir}/tests"
+    python -m unittest discover "${source_dir}/../tests"
 done

97d5a8a79c01bc0419239fb4f45c5d4fc4062450
Marco Ferrari <ferrarim@google.com>
2025-03-14 08:12:27 +0000

    fix: add missing vars

--- platforms/gke/base/use-cases/federated-learning/deploy.sh
+++ platforms/gke/base/use-cases/federated-learning/deploy.sh
@@ -18,6 +18,13 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+# Doesn't follow symlinks, but it's likely expected for most users
+SCRIPT_BASENAME="$(basename "${0}")"
+SCRIPT_DIRECTORY_PATH="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+
+echo "This script (${SCRIPT_BASENAME}) has been invoked with: $0 $*"
+echo "This script directory path is: ${SCRIPT_DIRECTORY_PATH}"
+
 ACP_REPO_DIR="$(readlink -f "${SCRIPT_DIRECTORY_PATH}/../../../../../../")"
 export ACP_REPO_DIR
 export ACP_PLATFORM_BASE_DIR="${ACP_REPO_DIR}/platforms/gke/base"

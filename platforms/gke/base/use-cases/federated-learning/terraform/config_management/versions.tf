2c29c5191017554d47d61120fda7106df2c9c052
Aaron Rueth <rueth@google.com>
2025-04-10 15:39:40 +0000

    Updated Google providers

--- platforms/gke/base/use-cases/federated-learning/terraform/config_management/versions.tf
+++ platforms/gke/base/use-cases/federated-learning/terraform/config_management/versions.tf
@@ -18,7 +18,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "6.12.0"
+      version = "6.29.0"
     }
     local = {
       source  = "hashicorp/local"

31f60d6391a89c579b4d54b2413f2afa907da14a
Aaron Rueth <rueth@google.com>
2025-04-24 21:46:52 +0000

    Added PriorityClasses

---
+++ platforms/gke/base/core/workloads/priority_class/main.tf
@@ -0,0 +1,64 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+locals {
+  kubeconfig_directory = "${path.module}/../../../kubernetes/kubeconfig"
+  kubeconfig_file      = "${local.kubeconfig_directory}/${local.kubeconfig_file_name}"
+
+  manifests_directory          = "${local.manifests_directory_root}/cluster/priority_class"
+  manifests_directory_root     = "${path.module}/../../../kubernetes/manifests"
+  template_manifests_directory = "${path.module}/manifests/priority_class"
+}
+
+data "local_file" "kubeconfig" {
+  filename = local.kubeconfig_file
+}
+
+resource "terraform_data" "manifests" {
+  input = {
+    manifests_directory          = local.manifests_directory
+    template_manifests_directory = local.template_manifests_directory
+  }
+
+  provisioner "local-exec" {
+    command     = <<EOT
+mkdir -p "${self.input.template_manifests_directory}" && \
+mkdir -p "${self.input.manifests_directory}" && \
+cp -r templates/manifests/* "${self.input.template_manifests_directory}/" && \
+cp -r "${self.input.template_manifests_directory}"/* "${self.input.manifests_directory}/"
+EOT
+    interpreter = ["bash", "-c"]
+    working_dir = path.module
+  }
+
+  triggers_replace = {
+    manifests_directory          = local.manifests_directory
+    template_manifests_directory = local.template_manifests_directory
+  }
+}
+
+module "kubectl_apply_manifests" {
+  depends_on = [
+    terraform_data.manifests,
+  ]
+
+  source = "../../../modules/kubectl_apply"
+
+  apply_server_side           = true
+  kubeconfig_file             = data.local_file.kubeconfig.filename
+  manifest                    = local.template_manifests_directory
+  manifest_includes_namespace = true
+  recursive                   = true
+  use_kustomize               = false
+}

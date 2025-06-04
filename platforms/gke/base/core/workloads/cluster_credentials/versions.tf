37e0870a3fe9897a60e474a17cdda67c6760c311
Aaron Rueth <rueth@google.com>
2025-06-04 18:40:11 +0000

    Updated Terraform providers

--- platforms/gke/base/core/workloads/cluster_credentials/versions.tf
+++ platforms/gke/base/core/workloads/cluster_credentials/versions.tf
@@ -16,6 +16,10 @@ terraform {
   required_version = ">= 1.5.7"
 
   required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.38.0"
+    }
   }
 
   provider_meta "google" {

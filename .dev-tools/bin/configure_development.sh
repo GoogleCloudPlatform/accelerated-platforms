1c7215ce8b28c555462debf625c48c06541963fe
Aaron Rueth <rueth@google.com>
2025-06-11 10:54:34 -0700

    Added some development tools for repository management (#178)

---
+++ .dev-tools/bin/configure_development.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+MY_PATH="$(
+  cd "$(dirname "$0")" >/dev/null 2>&1
+  pwd -P
+)"
+
+source "${MY_PATH}/helpers/git.sh"
+
+ACP_REPO_DIR="$(realpath ${MY_PATH}/../../)"
+
+for file in "${git_assume_unchanged_files[@]}"; do
+  echo "assuming-unchanged ${file}"
+  git update-index --assume-unchanged ${ACP_REPO_DIR}/${file}
+done
+
+echo "Adding the .dev-tools/.gitignore file"
+cd "${ACP_REPO_DIR}" &&
+  git config core.excludesfile .dev-tools/.gitignore &&
+  cd - &>/dev/null

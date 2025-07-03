06c55bde879ae8e1e1584ea3dfdafabf510c2fd9
Laurent Grangeau <laurent.grangeau@gmail.com>
2025-07-03 13:58:09 +0200

    Add secret_manager module for cross-device example (#191)
    Co-authored-by: Laurent Grangeau <lgrangeau@google.com>
    Co-authored-by: Aaron Rueth <rueth@google.com>

--- platforms/gke/base/use-cases/federated-learning/examples/cross-device/setup-environment.sh
+++ platforms/gke/base/use-cases/federated-learning/examples/cross-device/setup-environment.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Copyright 2024 Google LLC
+# Copyright 2025 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -40,12 +40,19 @@ fi
 source "${ACP_PLATFORM_BASE_DIR}/use-cases/federated-learning/common.sh"
 
 echo "Setting up the environment for the cross-device federated learning example"
+CROSS_DEVICE_EXAMPLE_TENANT_NAME="fl-1"
+CROSS_DEVICE_MODEL_BUCKET="model-0"
+CROSS_DEVICE_AGGREGATED_GRADIENT_BUCKET="aggregated-gradient-0"
+CROSS_DEVICE_CLIENT_GRADIENT_BUCKET="client-gradient-0"
 
 # shellcheck disable=SC2034 # Variable is used in other scripts
 FEDERATED_LEARNING_CROSS_DEVICE_EXAMPLE_CONFIG_AUTO_VARS_FILE="${FEDERATED_LEARNING_SHARED_CONFIG_DIR}/uc_federated_learning_cross_device_example.auto.tfvars"
 
 # shellcheck disable=SC2034 # Variable is used in other scripts
 CROSS_DEVICE_EXAMPLE_TERRAFORM_INIT_CONFIGURATION_VARIABLES=(
+  "federated_learning_cloud_storage_buckets = {\"${CROSS_DEVICE_MODEL_BUCKET}\"={force_destroy=true,versioning_enabled=false,public_access_prevention=\"enforced\"},\"${CROSS_DEVICE_AGGREGATED_GRADIENT_BUCKET}\"={force_destroy=true,versioning_enabled=false,public_access_prevention=\"enforced\"},\"${CROSS_DEVICE_CLIENT_GRADIENT_BUCKET}\"={force_destroy=true,versioning_enabled=false,public_access_prevention=\"enforced\"}}"
+  "federated_learning_cloud_storage_buckets_iam_bindings = [{bucket_name=\"${CROSS_DEVICE_MODEL_BUCKET}\",member=\"federated_learning_cross_device_apps_service_account_placeholder\",role=\"roles/storage.objectUser\"},{bucket_name=\"${CROSS_DEVICE_AGGREGATED_GRADIENT_BUCKET}\",member=\"federated_learning_cross_device_apps_service_account_placeholder\",role=\"roles/storage.objectUser\"},{bucket_name=\"${CROSS_DEVICE_CLIENT_GRADIENT_BUCKET}\",member=\"federated_learning_cross_device_apps_service_account_placeholder\",role=\"roles/storage.objectUser\"}]"
+  "federated_learning_tenant_names = [\"${CROSS_DEVICE_EXAMPLE_TENANT_NAME}\"]"
 )
 
 # shellcheck disable=SC2034 # Variable is used in other scripts
@@ -54,10 +61,22 @@ CROSS_DEVICE_EXAMPLE_TERRAFORM_FEDERATED_LEARNING_USE_CASE_CONFIGURATION_VARIABL
 
 # shellcheck disable=SC2034 # Variable is used in other scripts
 CROSS_DEVICE_EXAMPLE_TERRAFORM_CONFIGURATION_VARIABLES=(
+  "federated_learning_cross_device_example_model_bucket = \"${CROSS_DEVICE_MODEL_BUCKET}\""
+  "federated_learning_cross_device_example_aggregated_gradient_bucket = \"${CROSS_DEVICE_AGGREGATED_GRADIENT_BUCKET}\""
+  "federated_learning_cross_device_example_client_gradient_bucket = \"${CROSS_DEVICE_CLIENT_GRADIENT_BUCKET}\""
+  "federated_learning_cross_device_example_encryption_key_service_a_base_url = \"https://privatekeyservice-ca-staging.rb-odp-key-host-dev.com/v1alpha\""
+  "federated_learning_cross_device_example_encryption_key_service_b_base_url = \"https://privatekeyservice-cb-staging.rb-odp-key-host-dev.com/v1alpha\""
+  "federated_learning_cross_device_example_encryption_key_service_a_cloudfunction_url = \"https://ca-staging-us-central1-encryption-key-service-clo-2q6l4c4evq-uc.a.run.app\""
+  "federated_learning_cross_device_example_encryption_key_service_b_cloudfunction_url = \"https://cb-staging-us-central1-encryption-key-service-clo-2q6l4c4evq-uc.a.run.app\""
+  "federated_learning_cross_device_example_wip_provider_a = \"projects/586348853457/locations/global/workloadIdentityPools/ca-staging-opwip-1/providers/ca-staging-opwip-pvdr-1\""
+  "federated_learning_cross_device_example_wip_provider_b = \"projects/586348853457/locations/global/workloadIdentityPools/cb-staging-opwip-1/providers/cb-staging-opwip-pvdr-1\""
+  "federated_learning_cross_device_example_service_account_a = \"ca-staging-opverifiedusr@rb-odp-key-host.iam.gserviceaccount.com\""
+  "federated_learning_cross_device_example_service_account_b = \"cb-staging-opverifiedusr@rb-odp-key-host.iam.gserviceaccount.com\""
 )
 
 # shellcheck disable=SC2034 # Variable is used in other scripts
 cross_device_example_terraservices=(
+  "secret_manager"
 )
 
 load_fl_terraform_outputs() {
---
+++ platforms/gke/base/use-cases/federated-learning/terraform/_shared_config/uc_federated_learning_example_cross_device_variables.tf
@@ -0,0 +1,179 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+locals {
+  federated_learning_cross_device_example_spanner_instance_name      = join("-", [local.unique_identifier_prefix, var.federated_learning_cross_device_example_spanner_instance_name])
+  federated_learning_cross_device_example_spanner_database_name      = join("-", [local.unique_identifier_prefix, var.federated_learning_cross_device_example_spanner_database_name])
+  federated_learning_cross_device_example_spanner_lock_database_name = join("-", [local.unique_identifier_prefix, var.federated_learning_cross_device_example_spanner_lock_database_name])
+
+  federated_learning_cross_device_example_aggregator_pubsub_name                       = var.federated_learning_cross_device_example_aggregator_pubsub_name
+  federated_learning_cross_device_example_aggregator_pubsub_topic                      = join("-", [local.unique_identifier_prefix, local.federated_learning_cross_device_example_aggregator_pubsub_name, "topic"])
+  federated_learning_cross_device_example_aggregator_pubsub_subscription               = join("-", [local.unique_identifier_prefix, local.federated_learning_cross_device_example_aggregator_pubsub_name, "subscription"])
+  federated_learning_cross_device_example_modelupdater_pubsub_name                     = var.federated_learning_cross_device_example_modelupdater_pubsub_name
+  federated_learning_cross_device_example_modelupdater_pubsub_topic                    = join("-", [local.unique_identifier_prefix, local.federated_learning_cross_device_example_modelupdater_pubsub_name, "topic"])
+  federated_learning_cross_device_example_modelupdater_pubsub_subscription             = join("-", [local.unique_identifier_prefix, local.federated_learning_cross_device_example_modelupdater_pubsub_name, "subscription"])
+  federated_learning_cross_device_example_aggregator_notifications_pubsub_name         = join("-", [local.federated_learning_cross_device_example_aggregator_pubsub_name, "notifications"])
+  federated_learning_cross_device_example_aggregator_notifications_pubsub_topic        = join("-", [local.unique_identifier_prefix, local.federated_learning_cross_device_example_aggregator_notifications_pubsub_name, "topic"])
+  federated_learning_cross_device_example_aggregator_notifications_pubsub_subscription = join("-", [local.unique_identifier_prefix, local.federated_learning_cross_device_example_aggregator_notifications_pubsub_name, "subscription"])
+
+  federated_learning_pubsub_topics = [local.federated_learning_cross_device_example_aggregator_pubsub_name, local.federated_learning_cross_device_example_modelupdater_pubsub_name, local.federated_learning_cross_device_example_aggregator_notifications_pubsub_name]
+
+  federated_learning_cross_device_example_client_gradient_bucket_name     = join("-", [local.unique_identifier_prefix, var.federated_learning_cross_device_example_client_gradient_bucket])
+  federated_learning_cross_device_example_aggregated_gradient_bucket_name = join("-", [local.unique_identifier_prefix, var.federated_learning_cross_device_example_aggregated_gradient_bucket])
+  federated_learning_cross_device_example_model_bucket_name               = join("-", [local.unique_identifier_prefix, var.federated_learning_cross_device_example_model_bucket])
+}
+
+## Federated Learning bucket names
+variable "federated_learning_cross_device_example_client_gradient_bucket" {
+  description = "Client gradient bucket name"
+  type        = string
+}
+
+variable "federated_learning_cross_device_example_aggregated_gradient_bucket" {
+  description = "Aggregated gradient bucket name"
+  type        = string
+}
+
+variable "federated_learning_cross_device_example_model_bucket" {
+  description = "Model bucket name"
+  type        = string
+}
+
+## Federated Learning Spanner variables
+variable "federated_learning_cross_device_example_spanner_instance_name" {
+  description = "Name of the Spanner instance"
+  type        = string
+  default     = "instance"
+}
+
+variable "federated_learning_cross_device_example_spanner_database_name" {
+  description = "Name of the Spanner database"
+  type        = string
+  default     = "database"
+}
+
+variable "federated_learning_cross_device_example_spanner_lock_database_name" {
+  description = "Name of the Spanner lock database"
+  type        = string
+  default     = "lock-database"
+}
+
+## Federated Learning pubsub variables
+variable "federated_learning_cross_device_example_aggregator_pubsub_name" {
+  description = "Aggregator topic to be created for the cross-device example"
+  type        = string
+  default     = "aggregator"
+}
+
+variable "federated_learning_cross_device_example_modelupdater_pubsub_name" {
+  description = "Modelupdater topic to be created for the cross-device example"
+  type        = string
+  default     = "modelupdater"
+}
+
+## Keys service
+variable "federated_learning_cross_device_example_encryption_key_service_a_base_url" {
+  description = "The base url of the encryption key service A."
+  type        = string
+}
+
+variable "federated_learning_cross_device_example_encryption_key_service_b_base_url" {
+  description = "The base url of the encryption key service B."
+  type        = string
+}
+
+variable "federated_learning_cross_device_example_encryption_key_service_a_cloudfunction_url" {
+  description = "The cloudfunction url of the encryption key service A."
+  type        = string
+}
+
+variable "federated_learning_cross_device_example_encryption_key_service_b_cloudfunction_url" {
+  description = "The cloudfunction url of the encryption key service B."
+  type        = string
+}
+
+variable "federated_learning_cross_device_example_wip_provider_a" {
+  description = "The workload identity provider of the encryption key service A."
+  type        = string
+}
+
+variable "federated_learning_cross_device_example_wip_provider_b" {
+  description = "The workload identity provider of the encryption key service B."
+  type        = string
+}
+
+variable "federated_learning_cross_device_example_service_account_a" {
+  description = "The service account to impersonate of the encryption key service A."
+  type        = string
+}
+
+variable "federated_learning_cross_device_example_service_account_b" {
+  description = "The service account to impersonate of the encryption key service B."
+  type        = string
+}
+
+## Misc
+variable "federated_learning_cross_device_example_download_plan_token_duration" {
+  description = "Duration in seconds the download plan signed URL token is valid for"
+  type        = number
+  default     = 900
+}
+
+variable "federated_learning_cross_device_example_download_checkpoint_token_duration" {
+  description = "Duration in seconds the download checkpoint signed URL token is valid for"
+  type        = number
+  default     = 900
+}
+
+variable "federated_learning_cross_device_example_upload_gradient_token_duration" {
+  description = "Duration in seconds the upload gradient signed URL token is valid for"
+  type        = number
+  default     = 900
+}
+
+variable "federated_learning_cross_device_example_allow_rooted_devices" {
+  description = "Whether to allow rooted devices. This setting will have no effect when authentication is disabled. It is recommended to be set false for production environments."
+  type        = bool
+  default     = false
+}
+
+variable "federated_learning_cross_device_example_is_authentication_enabled" {
+  description = "Whether to enable authentication"
+  type        = bool
+  default     = false
+}
+
+variable "federated_learning_cross_device_example_local_compute_timeout_minutes" {
+  description = "The duration an assignment will remain in ASSIGNED status before timing out in minutes."
+  type        = number
+  default     = 15
+}
+
+variable "federated_learning_cross_device_example_upload_timeout_minutes" {
+  description = "The duration an assignment will remain in LOCAL_COMPLETED status before timing out in minutes."
+  type        = number
+  default     = 15
+}
+
+variable "federated_learning_cross_device_example_aggregation_batch_failure_threshold" {
+  description = "The number of aggregation batches failed for an iteration before moving the iteration to a failure state."
+  type        = number
+  default     = 3
+}
+
+variable "federated_learning_cross_device_example_collector_batch_size" {
+  description = "The size of aggregation batches created by the collector"
+  type        = number
+  default     = 50
+}

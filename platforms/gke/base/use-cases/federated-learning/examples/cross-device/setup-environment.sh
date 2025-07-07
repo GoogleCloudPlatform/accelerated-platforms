92333f8fe19bb75f9e1960df1bdd204e012f9f70
Laurent Grangeau <lgrangeau@google.com>
2025-07-07 18:09:05 +0200

    feat: add cloudbuild terraservice for the workload images

--- platforms/gke/base/use-cases/federated-learning/examples/cross-device/setup-environment.sh
+++ platforms/gke/base/use-cases/federated-learning/examples/cross-device/setup-environment.sh
@@ -81,6 +81,11 @@ CROSS_DEVICE_EXAMPLE_TERRAFORM_CONFIGURATION_VARIABLES=(
 )
 
 # shellcheck disable=SC2034 # Variable is used in other scripts
+cross_device_example_prerequisites_terraservices=(
+  "cloud_build"
+)
+
+# shellcheck disable=SC2034 # Variable is used in other scripts
 cross_device_example_terraservices=(
   "confidential_space"
   "secret_manager"

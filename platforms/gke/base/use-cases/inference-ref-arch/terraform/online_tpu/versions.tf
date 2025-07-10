b184f171e682af614decfe4a990bab38340616ea
Aaron Rueth <rueth@google.com>
2025-07-10 16:10:30 -0700

    Created online GPU Terraservice and integrated Hugging Face Terraservice (#203)

--- platforms/gke/base/use-cases/inference-ref-arch/terraform/online_tpu/versions.tf
+++ platforms/gke/base/use-cases/inference-ref-arch/terraform/online_tpu/versions.tf
@@ -20,10 +20,6 @@ terraform {
       source  = "hashicorp/google"
       version = "6.38.0"
     }
-    local = {
-      source  = "hashicorp/local"
-      version = "2.5.3"
-    }
   }
 
   provider_meta "google" {

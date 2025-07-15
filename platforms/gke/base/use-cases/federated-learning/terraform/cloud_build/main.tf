bb1fcad6cd45dc68e85f6610d8d138a8483afdcf
Laurent Grangeau <lgrangeau@google.com>
2025-07-15 11:17:40 +0200

    fix: pr review

--- platforms/gke/base/use-cases/federated-learning/terraform/cloud_build/main.tf
+++ platforms/gke/base/use-cases/federated-learning/terraform/cloud_build/main.tf
@@ -13,7 +13,8 @@
 # limitations under the License.
 
 locals {
-  cloudbuild_submit_command = "gcloud builds submit --substitutions=_PROJECT_ID=\"${google_project_service.cloud_build_googleapis_com.project},_REGISTRY=europe-docker.pkg.dev/${google_project_service.cloud_build_googleapis_com.project}/container-image-repository\" --region ${var.cluster_region}"
+  cloudbuild_submit_command    = "gcloud builds submit --substitutions=_PROJECT_ID=\"${google_project_service.cloud_build_googleapis_com.project},_REGISTRY=${var.cluster_region}-docker.pkg.dev/${google_project_service.cloud_build_googleapis_com.project}/${var.federated_learning_cross_device_example_repository_name}\" --region ${var.cluster_region}"
+  cloudbuild_git_clone_command = "git clone --recurse-submodules https://github.com/privacysandbox/odp-federatedcompute --branch=${var.federated_learning_cross_device_example_federatedcompute_tag}"
   cloudbuild_sa_roles = [
     "roles/storage.objectUser",
     "roles/logging.logWriter",
@@ -21,13 +22,13 @@ locals {
   ]
 }
 
-resource "google_cloudbuild_worker_pool" "odp_federatedcompute_privatepool" {
-  name     = "odp-federatedcompute-privatepool"
+resource "google_cloudbuild_worker_pool" "privatepool" {
+  name     = "privatepool"
   location = var.cluster_region
 
   worker_config {
     machine_type   = "e2-standard-32"
-    no_external_ip = false
+    no_external_ip = true
   }
 }
 
@@ -45,7 +46,8 @@ resource "google_project_iam_member" "cloudbuild_sa_roles" {
 
 resource "terraform_data" "build_workloads_images" {
   input = {
-    git_clone_command         = "git clone --recurse-submodules https://github.com/privacysandbox/odp-federatedcompute"
+    # Checkout a specific tag to make the build reproducible
+    git_clone_command         = local.cloudbuild_git_clone_command
     cloudbuild_submit_command = local.cloudbuild_submit_command
   }
 
@@ -54,15 +56,16 @@ resource "terraform_data" "build_workloads_images" {
 ${self.input.git_clone_command}
 ${self.input.cloudbuild_submit_command}
 EOT
-    interpreter = ["bash", "-c"]
+    interpreter = ["bash", "-o", "errexit", "-c"]
     working_dir = path.module
   }
 
   triggers_replace = {
-    always_run = timestamp()
+    federatedcompute_tag = var.federated_learning_cross_device_example_federatedcompute_tag
+    cloudbuild_sa_email  = google_project_service_identity.cloudbuild_sa.email
   }
 
   depends_on = [
-    google_cloudbuild_worker_pool.odp_federatedcompute_privatepool
+    google_cloudbuild_worker_pool.privatepool
   ]
 }

151440c9c1f895611949c6cbdfeedb9a04861b10
Aaron Rueth <rueth@google.com>
2025-08-26 22:25:08 +0000

    Fixed issues

--- platforms/gke/base/use-cases/inference-ref-arch/terraform/_shared_config/scripts/set_environment_variables.sh
+++ platforms/gke/base/use-cases/inference-ref-arch/terraform/_shared_config/scripts/set_environment_variables.sh
@@ -31,8 +31,11 @@ export SHARED_CONFIG_PATHS
 source "${ACP_PLATFORM_BASE_DIR}/_shared_config/scripts/set_environment_variables.sh"
 
 if [[ -v HF_MODEL_ID ]]; then
-  export HF_MODEL_ID_HASH=$(echo "${HF_MODEL_ID}" | md5sum | cut -c1-8)
+  HF_MODEL_ID_HASH=$(echo "${HF_MODEL_ID}" | md5sum | cut -c1-8)
+  export HF_MODEL_ID_HASH
 
   HF_MODEL_NAME="${HF_MODEL_ID##*/}"
-  export HF_MODEL_NAME="${HF_MODEL_NAME,,}"
+  HF_MODEL_NAME="${HF_MODEL_NAME//./-}"
+  HF_MODEL_NAME="${HF_MODEL_NAME,,}"
+  export HF_MODEL_NAME
 fi

318292adbc78713b25c874370590363bbea98e92
aburhan <ameenah.burhan@gmail.com>
2025-08-29 14:23:34 +0000

    add iam dependency

--- platforms/gke/base/use-cases/inference-ref-arch/terraform/comfyui/iam.tf
+++ platforms/gke/base/use-cases/inference-ref-arch/terraform/comfyui/iam.tf
@@ -58,6 +58,6 @@ resource "google_storage_bucket_iam_member" "ai_service_agent_gcs_role" {
   bucket     = each.key
   member     = "serviceAccount:${google_project_service_identity.aiplatform_service_agents.email}"
   role       = "roles/storage.objectUser"
-  depends_on = [google_project_service_identity.aiplatform_service_agents]
+  depends_on = [google_project_service_identity.aiplatform_service_agents, google_project_service.aiplatform_googleapis_com]
 }
 

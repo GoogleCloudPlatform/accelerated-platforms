27213b1d6042ee8a21bf15373a608974ec67c761
Ameenah Burhan <ameenah.burhan@gmail.com>
2025-09-24 23:13:09 -0400

    Update run-comfyui-tests.sh

--- test/ci-cd/scripts/comfyui/run-comfyui-tests.sh
+++ test/ci-cd/scripts/comfyui/run-comfyui-tests.sh
@@ -81,6 +81,7 @@ gcloud builds submit \
 --project="${cluster_project_id}" \
 --service-account="${comfyui_cloudbuild_service_account_id}" \
 --substitutions="_BUCKET_NAME=${comfyui_cloud_storage_model_bucket_name}"
+
 # ------------------------------------------------------------
 # Get GKE credentials
 # ------------------------------------------------------------

0046b40765f330914caa34013dbcf52b831cd70a
Mofi Rahman <mofi@google.com>
2025-10-02 19:57:53 -0400

    reformat code

--- modules/python/src/custom_nodes/google_genmedia/gemini_flash_image_node.py
+++ modules/python/src/custom_nodes/google_genmedia/gemini_flash_image_node.py
@@ -46,9 +46,7 @@ class Gemini25FlashImage:
             "required": {
                 "model": (
                     [model.name for model in GeminiFlashImageModel],
-                    {
-                        "default": GeminiFlashImageModel.GEMINI_25_FLASH_IMAGE.name
-                    },
+                    {"default": GeminiFlashImageModel.GEMINI_25_FLASH_IMAGE.name},
                 ),
                 "prompt": (
                     "STRING",

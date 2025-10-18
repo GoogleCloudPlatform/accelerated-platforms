e7ec79f7833dac788d74132a5c9c736c1ea7e3d0
Marlon <73705714+rumarlon@users.noreply.github.com>
2025-10-18 00:35:57 -0400

    update veo3 interpolation, fix veo3 i2v

--- platforms/gke/base/use-cases/inference-ref-arch/terraform/comfyui/gcs.tf
+++ platforms/gke/base/use-cases/inference-ref-arch/terraform/comfyui/gcs.tf
@@ -110,6 +110,18 @@ resource "google_storage_bucket_object" "veo3_gcsimage" {
   source = "src/comfyui-workflows/input-images/jellyfish.png"
 }
 
+resource "google_storage_bucket_object" "veo3_gcsimage" {
+  bucket = google_storage_bucket.comfyui_input.name
+  name   = "singer_start_frame.png"
+  source = "src/comfyui-workflows/input-images/singer_start_frame.png"
+}
+
+resource "google_storage_bucket_object" "veo3_gcsimage" {
+  bucket = google_storage_bucket.comfyui_input.name
+  name   = "singer_end_frame.png"
+  source = "src/comfyui-workflows/input-images/singer_end_frame.png"
+}
+
 resource "google_storage_bucket_object" "vto_gcsimage" {
   for_each = local.vto_files_to_upload
   bucket   = google_storage_bucket.comfyui_input.name

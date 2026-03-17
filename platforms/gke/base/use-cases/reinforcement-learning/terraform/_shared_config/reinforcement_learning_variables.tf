59ec65f95232786977735359572eeaaa34b81d01
Laurent Grangeau <lgrangeau@google.com>
2026-03-17 13:09:15 +0000

    feat: renamin variable for consistency

--- platforms/gke/base/use-cases/reinforcement-learning/terraform/_shared_config/reinforcement_learning_variables.tf
+++ platforms/gke/base/use-cases/reinforcement-learning/terraform/_shared_config/reinforcement_learning_variables.tf
@@ -19,7 +19,9 @@ locals {
   rl_kubernetes_service_account_name          = var.rl_kubernetes_service_account_name != null ? var.rl_kubernetes_service_account_name : "${local.unique_identifier_prefix}-rl-sa"
   rl_mlflow_data_bucket_name                  = var.rl_mlflow_data_bucket_name != null ? var.rl_mlflow_data_bucket_name : "${local.rl_project_id}-${local.unique_identifier_prefix}-mlflow-data"
   rl_project_id                               = var.rl_project_id != null ? var.rl_project_id : var.platform_default_project_id
-  rl_tpu_rl_on_tpu_image_url                  = var.rl_tpu_rl_on_tpu_image_url != null ? var.rl_tpu_rl_on_tpu_image_url : "${local.cloudbuild_ar_image_repository_url}/reinforcement-learning/rl-on-tpu:latest"
+
+  rl_cpu_reinforcement_learning_dataset_downloader_image_url = var.rl_cpu_reinforcement_learning_dataset_downloader_image_url != null ? var.rl_cpu_reinforcement_learning_dataset_downloader_image_url : "${local.cloudbuild_ar_image_repository_url}/reinforcement-learning/rl-dataset-downloader:latest"
+  rl_tpu_reinforcement_learning_on_tpu_image_url             = var.rl_tpu_reinforcement_learning_on_tpu_image_url != null ? var.rl_tpu_reinforcement_learning_on_tpu_image_url : "${local.cloudbuild_ar_image_repository_url}/reinforcement-learning/rl-on-tpu:latest"
 
   rl_kubernetes_service_accounts = {
     mlflow = {
@@ -59,7 +61,13 @@ variable "rl_project_id" {
   type        = string
 }
 
-variable "rl_tpu_rl_on_tpu_image_url" {
+variable "rl_cpu_reinforcement_learning_dataset_downloader_image_url" {
+  default     = null
+  description = "The URL for the RL dataset downloader container image."
+  type        = string
+}
+
+variable "rl_tpu_reinforcement_learning_on_tpu_image_url" {
   default     = null
   description = "The URL for the RL on TPU container image."
   type        = string

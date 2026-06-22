d88fc6d67fe13db79e27af11cdfe3961a0e6dd32
laurentgrangeau <lgrangeau@google.com>
2026-06-22 14:47:25 +0200

    fix: update to minimum version for machine learning diagnostics

--- platforms/gke/base/core/container_cluster/versions.tf
+++ platforms/gke/base/core/container_cluster/versions.tf
@@ -18,11 +18,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "7.6.0"
+      version = "7.28.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "7.6.0"
+      version = "7.28.0"
     }
   }
 

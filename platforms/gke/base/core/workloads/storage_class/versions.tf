0f83acd4d96de56d4141fd0c3c87379b3840ac64
Ali Zaidi <raincloud@google.com>
2026-07-21 22:19:17 +0000

    chore: bump terraform provider google version to 7.39.0 in storage_class

--- platforms/gke/base/core/workloads/storage_class/versions.tf
+++ platforms/gke/base/core/workloads/storage_class/versions.tf
@@ -18,7 +18,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "7.6.0"
+      version = "7.39.0"
     }
     local = {
       source  = "hashicorp/local"

601bb58191f677b62fb09e76172cfa1294c2f272
Shobhit Gupta <gushob@google.com>
2026-08-03 20:37:26 +0000

    Adding lustre terraservice
    enabling lustre plugin on GKE
    
    temporary fix - Update platform_variables.tf to bypass CI failures
    
    temporary fix to move CI to run in us-east5
    
    restoring the region back to us-central1
    
    fixing the typo in the region
    
    making some temporary changes for CI to run in us-east4 and not run CCC checks
    
    fixing an old misconfig for east4 node_pools
    
    disabling  container_node_pool terraservice in teardown scripts to align them with deploy scripts
    
    harcoding genmedia custom nodes region to us-central1 that only has endpoints in that region
    
    fixing comfyui test  workflows since veo2 endpoint has been deprecated.
    
    resolving a race condition and extending comfyui pipeline
    
    making comfyui CI run only on cpu
    
    adding TF dependencies to avoid failures
    
    applying fixes to the cpu image of Comfyui and adding dependecy check on API enablement for lustre terraservice
    
    fixing unwanted code
    
    fixing plan sequence
    
    fixing comfyui failures
    
    fixing comfyui issues
    
    fixing cspell
    
    bumping up timeout

--- platforms/gke/base/_shared_config/platform_variables.tf
+++ platforms/gke/base/_shared_config/platform_variables.tf
@@ -52,7 +52,7 @@ variable "platform_default_project_id" {
 }
 
 variable "platform_default_region" {
-  default     = "us-central1"
+  default     = "us-east4"
   description = "Default region to create resources in if a more specific region is not specified."
   type        = string
 }

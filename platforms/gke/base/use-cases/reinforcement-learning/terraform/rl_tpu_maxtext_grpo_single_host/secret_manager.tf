7e0470af23a153fa9c50bd921b58508d242095db
Kavitha Rajendran <103603287+karajendran@users.noreply.github.com>
2026-08-06 03:35:22 -0700

    RL with TPU: Single Slice; Llama3.1 8B IT (#513)
    Co-authored-by: laurentgrangeau <lgrangeau@google.com>

--- platforms/gke/base/use-cases/reinforcement-learning/terraform/rl_tpu_maxtext_grpo_single_host/secret_manager.tf
+++ platforms/gke/base/use-cases/reinforcement-learning/terraform/rl_tpu_maxtext_grpo_single_host/secret_manager.tf
@@ -1,4 +1,4 @@
-# Copyright 2026 Google LLC
+# Copyright 2025 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,10 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-data "google_project" "cluster" {
-  project_id = local.cluster_project_id
+data "google_secret_manager_secret" "hub_access_token_read" {
+  project   = data.google_project.huggingface_secret_manager.project_id
+  secret_id = local.huggingface_hub_access_token_read_secret_manager_secret_name
 }
 
-data "google_project" "huggingface_secret_manager" {
-  project_id = local.huggingface_secret_manager_project_id
+data "google_secret_manager_secret" "hub_access_token_write" {
+  project   = data.google_project.huggingface_secret_manager.project_id
+  secret_id = local.huggingface_hub_access_token_write_secret_manager_secret_name
 }

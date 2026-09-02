50a69e11a67c5913a0f0c61c395f5a60009f2e58
Laurent Grangeau <lgrangeau@google.com>
2026-09-02 17:13:19 +0200

    fix: version mismatched for terraform providers (#515)

--- platforms/gke/base/use-cases/inference-ref-arch/examples/llmd/initialize/versions.tf
+++ platforms/gke/base/use-cases/inference-ref-arch/examples/llmd/initialize/versions.tf
@@ -18,11 +18,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "7.6.0"
-    }
-    google-beta = {
-      source  = "hashicorp/google-beta"
-      version = "7.6.0"
+      version = "7.41.0"
     }
     helm = {
       source  = "hashicorp/helm"
--- platforms/gke/base/use-cases/training-ref-arch/terraform/model_fine_tuning/versions.tf
+++ platforms/gke/base/use-cases/training-ref-arch/terraform/model_fine_tuning/versions.tf
@@ -18,7 +18,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "7.6.0"
+      version = "7.41.0"
     }
     helm = {
       source  = "hashicorp/helm"
@@ -26,7 +26,7 @@ terraform {
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "2.38.0"
+      version = "3.2.1"
     }
     local = {
       source  = "hashicorp/local"
